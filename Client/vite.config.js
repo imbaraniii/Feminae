cefe137756519cbf3d04742412e3ce43457fa613
HXMAN76 <hariheman76@gmail.com>
2024-12-29 10:28:03 +0530

    Deleted junk files

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,12 +1,17 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
- 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true, // Enable polling to handle file system watcher limits
     },
   },
 });
